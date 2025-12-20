e4f82c467a1d7f8151a94732cc2c78471535236a
Briamco <crossinerobriamco@gmail.com>
2025-12-20 10:51:41 -0400

    feat: add cursor pointer to card image for better interactivity

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 function Card({ imgUrl, covert }: CardProps) {
   return (
     <>
-      <img src={imgUrl} className={`rounded-2xl max-h-180 ${covert ? "" : "animate-little-bounce"}`} />
+      <img src={imgUrl} className={`rounded-2xl max-h-180 ${covert ? "" : "animate-little-bounce cursor-pointer"}`} />
     </>
   )
 }
